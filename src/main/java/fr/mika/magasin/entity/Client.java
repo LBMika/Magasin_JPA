9ffafd2191836dc12719d0e2a6ae454572e7cbfb
LMika <lemika.pro@gmail.com>
2021-12-13 04:03:46 +0100

    Client entity

---
+++ src/main/java/fr/mika/magasin/entity/Client.java
@@ -0,0 +1,22 @@
+package fr.mika.magasin.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="client")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Client {
+    @Id
+    @Column(name="id_client")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long clientId;
+    private String firstname;
+    private String lastname;
+}
