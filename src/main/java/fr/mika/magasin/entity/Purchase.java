c57728188c241e4ce9ef692331cd1ec2dd935d8a
LMika <lemika.pro@gmail.com>
2021-12-13 04:18:47 +0100

    Purchase entity

---
+++ src/main/java/fr/mika/magasin/entity/Purchase.java
@@ -0,0 +1,19 @@
+package fr.mika.magasin.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="purchase")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Purchase {
+    @Id
+    @Column(name="id_purchase")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long purchaseId;
+}
